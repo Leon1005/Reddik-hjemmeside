8f73d0000a96973f4687832abf6811fe94f1a02b
Leon Borg <110337866+Leon1005@users.noreply.github.com>
2022-08-07 17:16:03 +0200

    recreate header and import font

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,15 @@
-<h1>Hei</h1>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link
+    href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap"
+    rel="stylesheet"
+/>
+
+<div class="main">
+    <p>ReddikAS</p>
+    <h1>Vi bygger din nye hjemmeside</h1>
+    <p>En kort setning om hva vi gjør og hva som skiller oss fra andre.</p>
+
+    <button>Kontakt oss</button>
+</div>
